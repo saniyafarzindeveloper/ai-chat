cc1db8460635cbc36d02fd5318fd7d78c2b1a5d7
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-08-21 21:17:27 +0530

    openai handler file - in progres

---
+++ src/agents/openai/OpenAIResponseHandler.ts
@@ -0,0 +1,26 @@
+import OpenAI from "openai";
+import { AssistantStream } from "openai/lib/AssistantStream";
+import type { Channel, Event, MessageResponse, StreamChat } from "stream-chat";
+
+export class OpenAIResponseHandler {
+    //private → accessible only within the class.
+    //declare variavle + constructor
+    private message_text = ""
+    private message_chunk = 0 //to track the no of text chuncks recieved - for monitoring & debugging
+    private run_id = "" //unique id for openai - req for generation
+    private is_done = false //flag to check the status / progress
+    private last_update_time = 0
+
+    constructor(
+        private readonly openai : OpenAI,
+        private readonly openAiThread : OpenAI.Beta.Threads.Thread,
+        private readonly assistantStream : AssistantStream,
+        private readonly chatClient :  StreamChat,
+        private readonly channel: Channel,
+        private readonly message: MessageResponse,
+        private readonly onDisposel : () => void,
+    ){
+
+    }
+
+}
